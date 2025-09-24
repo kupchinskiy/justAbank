daccf350872366a0e706d2a7a7183e516eecc773
Valentin Kup <hellhound.first@gmail.com>
2025-09-24 18:23:58 +0300

    The appearance of parts of the site

--- script.js
+++ script.js
@@ -206,15 +206,37 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const getStickyNav = function (entries) {
   const entry = entries[0];
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) {
     nav.classList.add('sticky');
   } else nav.classList.remove('sticky');
 };
 
-const observer = new IntersectionObserver(getStickyNav, {
+const headerObserver = new IntersectionObserver(getStickyNav, {
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px`,
 });
-observer.observe(header);
+headerObserver.observe(header);
+
+//  Появление частей сайта
+
+const allSections = document.querySelectorAll('.section');
+
+const appearanceSection = function (entries, observer) {
+  const entry = entries[0];
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(appearanceSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});

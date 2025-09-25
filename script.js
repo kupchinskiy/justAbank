7c37e23b566ea2fd095d3eafab93e698265d39d2
Valentin Kup <hellhound.first@gmail.com>
2025-09-25 16:26:13 +0300

    feat: create dots slider

--- script.js
+++ script.js
@@ -274,6 +274,7 @@ const slides = document.querySelectorAll('.slide');
 
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let currentSlide = 0;
 const slidesNumber = slides.length;
@@ -281,6 +282,25 @@ const slidesNumber = slides.length;
 // const slider = document.querySelector('.slider');
 // slider.style.transform = 'scale(0.4) trnanslateX(1500px)';
 // slider.style.overflow = 'visible';
+
+const createDots = function () {
+  slides.forEach(function (_, index) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${index}"></button>`
+    );
+  });
+};
+createDots();
+const activateCurrentDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+activateCurrentDot(0);
 
 const moveToSlide = function (slide) {
   slides.forEach(
@@ -294,23 +314,50 @@ moveToSlide(0);
 //   (slide, index) => (slide.style.transform = `translateX(${index * 100}%)`)
 //   // 1-0%, 2-100%, 3-200%, 4-300%
 // );
-
-btnRight.addEventListener('click', function () {
+const nextSlide = function () {
   if (currentSlide === slidesNumber - 1) {
     currentSlide = 0;
   } else {
     currentSlide++;
   }
   moveToSlide(currentSlide);
-  // 1- -100%, 2-0%, 3-100%, 4-200%
-});
+  activateCurrentDot(currentSlide);
+};
 
-btnLeft.addEventListener('click', function () {
+const previousSlide = function () {
   if (currentSlide === 0) {
     currentSlide = slidesNumber - 1;
   } else {
     currentSlide--;
   }
   moveToSlide(currentSlide);
+  activateCurrentDot(currentSlide);
+};
+
+btnRight.addEventListener(
+  'click',
+  nextSlide
   // 1- -100%, 2-0%, 3-100%, 4-200%
+);
+
+btnLeft.addEventListener(
+  'click',
+  previousSlide
+
+  // 1- -100%, 2-0%, 3-100%, 4-200%
+);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') nextSlide();
+  if (e.key === 'ArrowLeft') previousSlide();
+});
+
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const slide = e.target.dataset.slide;
+    console.log(slide);
+    moveToSlide(slide);
+    console.log('hello');
+    activateCurrentDot(slide);
+  }
 });
